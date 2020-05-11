d933bab1cc456ef9e606e82641a625f452b293e3
Антон Ткачук <chiga2030@gmail.com>
2020-05-11 22:32:48 +0300

    gulp babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 
 moving = () => {
 	gulp.src('./source/**/*.js')
+		.pipe(babel({
+    	presets: ['@babel/env']
+    }))
 		.pipe(gulp.dest('./prod/'));
 
 	gulp.src('./source/**/*.css')
