7a97df2754e65a233e50006886f7686b52f2ef22
Антон Ткачук <chiga2030@gmail.com>
2020-05-12 23:11:28 +0300

    add plugin gulp-sourcemaps for js and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
+const path = {
+	build: './build/',
+};
 const gulp = require('gulp');
 const babel = require('gulp-babel');		//для трансшпиляции
 const concat = require('gulp-concat');	//для объединения файлов
 const uglify = require('gulp-uglify');	//для минификации js-файлов
 const cssnano = require('gulp-cssnano');	//для минификации css-файлов
+const sourcemaps = require('gulp-sourcemaps');
 
-moving = () => {
+gulp.task('build-js', () => {
 	gulp.src('./source/**/*.js')
-		.pipe(concat('all.js'))
-		.pipe(babel({
-    	presets: ['@babel/env']
-    }))
-    .pipe(uglify())
-		.pipe(gulp.dest('./prod/js/'));
+		.pipe(sourcemaps.init())
+			.pipe(concat('all.js'))
+			.pipe(babel({
+    		presets: ['@babel/env']
+    	}))
+    	.pipe(uglify())
+    .pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.build + 'js/'));
+});
 
+gulp.task('build-css', () => {
 	gulp.src('./source/**/*.css')
-		.pipe(concat('style.css'))
-		.pipe(cssnano())
-		.pipe(gulp.dest('./prod/css/'));
-}
+		.pipe(sourcemaps.init())
+			.pipe(concat('style.css'))
+			.pipe(cssnano())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.build + 'css/'));
+});
 
-gulp.task('mov', moving());
+gulp.task('default', ['build-js', 'build-css']);
--- prod/js/all.js
+++
@@ -1,1 +0,0 @@
-"use strict";var a="test";console.log("hello");
