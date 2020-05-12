adc4923aa4fdee613aae1e1e6705750f6fbc30e0
Антон Ткачук <chiga2030@gmail.com>
2020-05-12 22:00:47 +0300

    add plugin gulp-cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');		//для трансшпиляции
 const concat = require('gulp-concat');	//для объединения файлов
-const uglify = require('gulp-uglify');	//для минификации файлов
+const uglify = require('gulp-uglify');	//для минификации js-файлов
+const cssnano = require('gulp-cssnano');	//для минификации css-файлов
 
 moving = () => {
 	gulp.src('./source/**/*.js')
@@ -10,10 +11,12 @@ moving = () => {
     	presets: ['@babel/env']
     }))
     .pipe(uglify())
-		.pipe(gulp.dest('./prod/'));
+		.pipe(gulp.dest('./prod/js/'));
 
 	gulp.src('./source/**/*.css')
-		.pipe(gulp.dest('./prod/'));
+		.pipe(concat('style.css'))
+		.pipe(cssnano())
+		.pipe(gulp.dest('./prod/css/'));
 }
 
 gulp.task('mov', moving());
