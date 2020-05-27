b6f9abe181f4aa6a74afd541ddcaabe0334c3fd5
Антон Ткачук <chiga2030@gmail.com>
2020-05-27 22:59:09 +0300

    add gulp-env, gulp-if, gulp-clean

--- gulpfile.js
+++ gulpfile.js
@@ -10,35 +10,50 @@ const path = {
 };
 
 const gulp = require('gulp');
+const env = require('gulp-env');				//переменные окружения
+const clean = require('gulp-clean');		//zero configuration tool
 const babel = require('gulp-babel');		//для трансшпиляции
 const concat = require('gulp-concat');	//для объединения файлов
 const uglify = require('gulp-uglify');	//для минификации js-файлов
+const gulpif = require('gulp-if');			//для минификации js-файлов
 const cssnano = require('gulp-cssnano');	//для минификации css-файлов
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
-/* сборка скриптов в один файл *-min.js*/
+env ({
+	file: '.env',
+	type: 'ini',
+});
+
+/* сборка скриптов в один файл *-min.js */
 gulp.task('build-scripts', () => {
 	gulp.src(path.src.script)
-		.pipe(sourcemaps.init())
+    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.init()))
 			.pipe(concat('scripts-min.js'))
-			.pipe(babel({
+    	.pipe(gulpif(process.env.BABEL === 'switch-on', babel({
 	    	presets: ['@babel/env']
-	    }))
-    	.pipe(uglify())
-  	.pipe(sourcemaps.write())
+	    })))
+    	.pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
+    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.write()))
 		.pipe(gulp.dest(path.build.script));
 });
 
-/* сборка стилей в один файл style-min.css*/
+/* сборка стилей в один файл style-min.css */
 gulp.task('build-styles', () => {
 	gulp.src(path.src.style)
-		.pipe(sourcemaps.init())
+    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.init()))
 			.pipe(concat('style-min.css'))
-			.pipe(cssnano())
-		.pipe(sourcemaps.write())
+    	.pipe(gulpif(process.env.NODE_ENV === 'production', cssnano()))
+    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.write()))
 		.pipe(gulp.dest(path.build.style));
 });
+
+/* zero configuration */
+gulp.task('clean', () => {
+	gulp.src('./build', {read: false})
+  	.pipe(clean());
+});
+
 gulp.task('default', ['browser-sync']);
 gulp.task('build', ['build-styles', 'build-scripts']);
 gulp.task('prod', ['build']);	//build for prod
